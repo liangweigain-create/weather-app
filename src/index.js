4dd25ccde6baa8a7cb23816acd93c7868f044616
Leo <liangweigain@gmail.com>
2025-12-02 15:41:52 +0800

    剩余styling和部分元素

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 import { getWeather } from "./modules/getWeather.js";
-import { renderWeatherCurrent, renderWeatherToday } from "./modules/domInterface.js";
+import { renderCityName, renderWeatherCurrent, renderWeatherToday } from "./modules/domInterface.js";
+import "../src/styles/index.css";
 
-async function initWeatherApp(location) {
+
+document.addEventListener('DOMContentLoaded', () => {
+    const location = 'zhuhai';
+    handleSearch(location);
+
+    const input = document.querySelector('.search');
+    const searchBtn = document.querySelector('.search-btn');
+
+    searchBtn.addEventListener('click', () => {
+        const inputValue = input.value.trim();
+        if (!inputValue) throw new Error('wrong input');
+        handleSearch(inputValue);
+    })
+    input.addEventListener('keypress', (e) => {
+        if (!e.key === 'Enter') return;
+        if (e.key === 'Enter') {
+            const inputValue = input.value.trim();
+            if (!inputValue) throw new Error('wrong input');
+            handleSearch(inputValue);
+        }
+    })
+})
+
+
+async function handleSearch(location) {
     const todayContainer = document.querySelector('.today-weather-container');
     const currentContainer = document.querySelector('.current-weather-container');
-
+    const chineseName = document.querySelector('.city-name');
+    
     console.log('开始请求数据。。');
     const weatherData = await getWeather(location);
     console.log('请求数据结束，开始渲染。。。');
 
+    renderCityName(location, chineseName)
     renderWeatherToday(weatherData, todayContainer);
     renderWeatherCurrent(weatherData, currentContainer);
 }
-
-document.addEventListener('DOMContentLoaded', async () => {
-    initWeatherApp('Beijin');
-})
--- src/modules/domInterface.js
+++ src/modules/domInterface.js
@@ -4,6 +4,8 @@ import { format } from 'date-fns';
 // 如需中文本地化，引入对应的语言包
 import { zhCN } from 'date-fns/locale';
 
+import cityData from "../../city.json";
+
 export function renderWeatherToday(weatherData, domEl) {
     try {
         if (!weatherData?.todayConditions) throw new Error('今日天气数据缺失');
@@ -24,19 +26,19 @@ export function renderWeatherToday(weatherData, domEl) {
         const tempAveEl = createElementWithContent(
             'p',
             'temp',
-            `平均温度： ${fahrenheitToCelsius(weatherData.todayConditions.feelsLike, 2)}˚C`
+            `平均温度： ${fahrenheitToCelsius(weatherData.todayConditions.tempAve, 2)}˚C`
         )
 
         const tempMinEl = createElementWithContent(
             'p',
             'temp',
-            `最低温度： ${fahrenheitToCelsius(weatherData.todayConditions.feelsLike)}˚C`
+            `最低温度： ${fahrenheitToCelsius(weatherData.todayConditions.tempMin)}˚C`
         )
 
         const tempMaxEl = createElementWithContent(
             'p',
             'temp',
-            `最高温度： ${fahrenheitToCelsius(weatherData.todayConditions.feelsLike)}˚C`
+            `最高温度： ${fahrenheitToCelsius(weatherData.todayConditions.tempMax)}˚C`
         )
 
         domEl.innerHTML = '';
@@ -61,15 +63,14 @@ export function renderWeatherCurrent(weatherData, domEl) {
         const currentTempEl = createElementWithContent(
             'p',
             'temp',
-            `${fahrenheitToCelsius(weatherData.currentConditions.temp)}˚C`
+            `当前温度：${fahrenheitToCelsius(weatherData.currentConditions.temp)}˚C`
         )
 
         const feelsTempEl = createElementWithContent(
             'p',
             'temp',
-            `${fahrenheitToCelsius(weatherData.currentConditions.temp)}˚C`
+            `体感温度：${fahrenheitToCelsius(weatherData.currentConditions.feelsLike)}˚C`
         )
-
         domEl.innerHTML = '';
         domEl.append(dateTimeEl, currentTempEl, feelsTempEl);
     }catch (error) {
@@ -77,6 +78,15 @@ export function renderWeatherCurrent(weatherData, domEl) {
         throw error;
     }
 }
+
+export function renderCityName(userInput,domEl) {
+    const chineseName = translate(userInput);
+
+    domEl.innerHTML = '';
+    domEl.append(chineseName);
+}
+
+
 
 /**
  * 华氏度转摄氏度
@@ -99,4 +109,18 @@ function createElementWithContent(tag, className, textContent) {
     if (textContent) el.textContent = textContent;
 
     return el;
+}
+
+function translate(pinyin) {
+    const userInput = String(pinyin.trim());
+    if (!userInput) throw new Error('input is not valid!');
+    const formatedInput = userInput[0].toUpperCase() + userInput.slice(1).toLowerCase();
+    const initial = formatedInput[0];
+    console.log(formatedInput);
+
+    const matchedObj = cityData.find(item => item.initial === initial);
+    console.log(matchedObj);
+    const matchedCity = matchedObj.list.find(city => city.pinyin === formatedInput)
+    const matchedName = matchedCity ? matchedCity.name : formatedInput;
+    return matchedName;
 }
